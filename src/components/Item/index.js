af8b3bfd1b4977f05238a04b82c25848d9718fe3
Lautaro Chamorro <lautarochamorro72@gmail.com>
2023-03-28 21:22:39 -0300

    PrimeraEntrega+Chamorro

---
+++ src/components/Item/index.js
@@ -0,0 +1,13 @@
+
+const Item = () => {
+    return (
+    <div className="navBar">
+        <button>Login</button>
+        <h1>Championship</h1>
+        <button>Sign in</button>
+        
+    </div>
+    );
+}
+
+export default Item;
---
+++ src/components/container/productos.js
@@ -0,0 +1,5 @@
+const listProduct = () => {
+    return <h1>Productos</h1>;
+}
+
+export default listProduct;
